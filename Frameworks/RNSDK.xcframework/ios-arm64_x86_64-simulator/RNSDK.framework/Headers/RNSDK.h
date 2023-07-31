cd4b5927413c93232a729369bf5333e8897371cf
Tamim Attafi <tamim.attafi@i-link.pro>
2023-07-31 22:29:44 +0700

    init: publish first version 1.1.0

---
+++ Frameworks/RNSDK.xcframework/ios-arm64_x86_64-simulator/RNSDK.framework/Headers/RNSDK.h
@@ -0,0 +1,18 @@
+//
+//  RNSDK.h
+//  RNSDK
+//
+//  Created by Tamim Attafi on 31.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNSDK.
+FOUNDATION_EXPORT double RNSDKVersionNumber;
+
+//! Project version string for RNSDK.
+FOUNDATION_EXPORT const unsigned char RNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSDK/PublicHeader.h>
+
+
